2df8796992887a4e7af9f44c78c0d9a51995b733
John Evans <john.evans@doordash.com>
2026-02-18 08:16:23 -0800

    Initial Commit

---
+++ People Directory 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  People Directory 1
+//
+//  Created by John Evans on 2/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ People Directory 1/People_Directory_1App.swift
@@ -0,0 +1,17 @@
+//
+//  People_Directory_1App.swift
+//  People Directory 1
+//
+//  Created by John Evans on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct People_Directory_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
